a67134084e5ce7fd4637a948fe628f350e968a8a
Hubert Pompecki <hpompecki@gmail.com>
2015-11-17 21:03:55 +0000

    comlete game

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,10 +1,11 @@
-require "tic_tac_toe/version"
+require_relative "tic_tac_toe/version"
 
 module TicTacToe
   # Your code goes here...
 end
 
-require "tic_tac_toe/cell.rb"
-require "tic_tac_toe/player.rb"
-require "tic_tac_toe/board.rb"
-require "tic_tac_toe/core_extensions.rb"
+require_relative "tic_tac_toe/cell.rb"
+require_relative "tic_tac_toe/player.rb"
+require_relative "tic_tac_toe/board.rb"
+require_relative "tic_tac_toe/core_extensions.rb"
+require_relative "tic_tac_toe/game.rb"
